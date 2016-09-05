888d2ac6b794b7dc0e49dd2090408416ca83eada
Rajat <raj@xyz.com>
2016-09-05 16:07:25 +0530

    fact changed -ve no

--- factorial.cpp
+++ factorial.cpp
@@ -4,10 +4,11 @@ int factorial(int n){
 if(n<0){
 
 
-printf("Enter a positive number :");
+printf("Please enter a positive number :");
 return -1;
 }
-    if(n!=1){
+	
+	if(n!=1){
 	return(n * factorial(n-1));
     }
     else return 1;
