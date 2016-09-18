e037e83b5312b5d9a1198fb92fa2b94e5d27a70b
Rajat Mehra <rajat.123.mehra@gmail.com>
2016-09-19 00:02:16 +0530

    Removing Bugs

--- Add_Digit.cpp
+++ Add_Digit.cpp
@@ -4,18 +4,23 @@
 int main()
 {
    int n, sum = 0, remainder;
- 
+   int flag=1;
    printf("Enter an integer\n");
    scanf("%d",&n);
-    
+   if(n<0){
+   n*=-1;
+   flag = 0;
+   }
    while(n != 0)
    {
+      if(n<10 && !flag)
+      n*=-1;
       remainder = n % 10;
 	  printf("The Remainder is %d\n", remainder);
       sum = sum + remainder;
 	  printf("The Sum is %d\n", sum);
-      n = n / 10;
-	  printf("The n is %d\n", n);
+      n = n / 10; 
+	  printf("The n is %d\n", flag?n:n*-1);
    }
  //Displaying the sum
    printf("Sum of digits of entered number = %d\n",sum);
