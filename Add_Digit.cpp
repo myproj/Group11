4f7683758176ab9188e6d6c3b2aa968b90b007f0
Rajat Mehra <rajat.123.mehra@gmail.com>
2016-09-18 23:17:30 +0530

    Printf to cout

--- Add_Digit.cpp
+++ Add_Digit.cpp
@@ -1,23 +1,27 @@
-#include<stdio.h>
+//This program calculates the sum of digits of a number
+
+#include<cstdio>
+#include<iostream>
+using namespace std;
  
 int main()
 {
    int n, sum = 0, remainder;
  
-   printf("Enter an integer\n");
-   scanf("%d",&n);
+   cout<<"Enter an integer\n";
+   cin>>n;
     
-   while(n != 0)
+   while(n!= 0)
    {
       remainder = n % 10;
-	  printf("The Remainder is %d\n", remainder);
+	  cout<<"The Remainder is "<<remainder<<"\n";
       sum = sum + remainder;
-	  printf("The Sum is %d\n", sum);
+	  cout<<"The Sum is "<<sum<<"\n";
       n = n / 10;
-	  printf("The n is %d\n", n);
+	  cout<<"The n is "<<n<<"\n";
    }
  
-   printf("Sum of digits of entered number = %d\n",sum);
+   cout<<"Sum of digits of entered number = "<<sum<<"\n";
  
    return 0;
 }
